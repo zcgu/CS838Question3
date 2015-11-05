3b39ef3b9d8e0f2cbf8f6f876486ac210ec9a482
zcgu <zchenggu@gmail.com>
2015-11-05 17:33:22 -0600

    11/4

--- src/main/scala/test2.scala
+++ src/main/scala/test2.scala
@@ -31,13 +31,15 @@ class test2 {
     var flag = 0
     for (vd1 <- 0 to length-1)
       for (vd2 <- 0 to length-1) {
+        if(vd1 != vd2) {
         flag=0
         for (i <- 0 to VDarray(vd1)._2.length-1)
           for (j <- 0 to VDarray(vd2)._2.length-1)
             if(VDarray(vd1)._2(i).equals(VDarray(vd2)._2(j)))
               flag = 1
-        if (flag == 1){
-          EDarraybuffer+=new Edge(VDarray(vd1)._1,VDarray(vd2)._1,-1)
+        if (flag == 1) {
+          EDarraybuffer += new Edge(VDarray(vd1)._1, VDarray(vd2)._1, -1)
+        }
         }
       }
     val EDarray = new Array[Edge[Int]](EDarraybuffer.length)
@@ -50,10 +52,30 @@ class test2 {
     val graph = Graph(words, connect)
 
     //Q1
-    val num: RDD[(Int,Int)] = graph.triplets.map(triplet => (triplet.srcAttr.length,triplet.dstAttr.length))
-    num.filter{case(a,b) => a>b}.count
+    val Q1num = graph.triplets.map(triplet => (triplet.srcAttr.length,triplet.dstAttr.length)).filter{case(a,b) => a>b}.count
 
     //Q2
+    val Q2total: VertexRDD[(Int,Int)] = graph.aggregateMessages[(Int,Int)](
+      triplet => triplet.sendToSrc(1,triplet.srcAttr.length),
+      (a, b) => (a._1+b._1 , b._2)
+      )
+
+    def max(a: (VertexId,(Int,Int)), b: (VertexId,(Int,Int))) :(VertexId,(Int,Int)) ={
+      if(a._2._1 > b._2._1 || (a._2._1 == b._2._1 && a._2._2>b._2._2)) a else b
+    }
+
+    Q2total.reduce(max)
+
+    //Q3
+    val Q3total: VertexRDD[(Int,Double)] = graph.aggregateMessages[(Int,Double)](
+      triplet => triplet.sendToSrc(1,1.0*triplet.dstAttr.length),
+      (a, b) => (a._1+b._1 , a._2+b._2)
+      )
+    val Q3res: VertexRDD[Double] = Q3total.mapValues( (id, value) => value match { case (count, total) => total / count } )
+    Q3res.collect.foreach(println)
+
+    //Q4
+
 
 
   }
